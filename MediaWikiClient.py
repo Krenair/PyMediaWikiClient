1d748b5e414ec947c830ea1e45259fe2a7b2a6cf
Alex M <dimension-@live.co.uk>
2012-01-31 16:32:02 +0000

    Query seems to be working now.

--- MediaWikiClient.py
+++ MediaWikiClient.py
@@ -5,7 +5,8 @@ class MediaWikiClient:
         self.userAgent = userAgent
         self.cookieInfo = None
         self.isLoggedIn = False
-    
+        self.getUserInfo()
+
     def apiRequest(self, values, headers = {}, urlExtras = ''):
         """Handles all requests to MediaWiki"""
         from StringIO import StringIO
@@ -24,19 +25,19 @@ class MediaWikiClient:
         else:
             data = response.read()
         return json.loads(data)
-    
+
     def listToString(self, list):
         out = ''
         for item in list:
             out += (item + '|')
         return out[:-1]
-    
+
     def getUserInfo(self):
         properties = ['blockinfo', 'hasmsg', 'groups', 'implicitgroups', 'rights', 'changeablegroups', 'editcount', 'ratelimits', 'email', 'registrationdate']
-        result = self.apiRequest({'action':'query', 'meta':'userinfo', 'uiprop':self.listToString(properties)}) #fix this when the query method is implemented
+        result = self.query(meta = ['userinfo'], extraParams = {'uiprop':self.listToString(properties)})
         self.userInfo = result['query']['userinfo']
         return self.userInfo
-    
+
     def login(self, username, password):
         self.username = username
         #get login token
@@ -57,7 +58,7 @@ class MediaWikiClient:
             return True
         else:
             raise APIError, result
-    
+
     def logout(self):
         if self.isLoggedIn:
             self.apiRequest({'action':'logout'})
@@ -65,65 +66,79 @@ class MediaWikiClient:
             return True
         else:
             raise Exception, "Not logged in."
-    
+
     def query(self, titles = [], pageIds = [], revIds = [], list = [], meta = [], generator = '', redirects = True, convertTitles = False, indexPageIds = False, export = False, exportNoWrap = False, iwUrl = False, extraParams = {}):
-        values = {'action':'query', 'titles':self.listToString(titles), 'pageids':self.listToString(pageIds), 'revids':self.listToString(revIds), 'list':self.listToString(list), 'meta':self.listToString(meta), 'generator':generator}
-        for key, value in extraparams:
-            header[key] = value
-        
-        if redirect == True:
-            values['redirect'] = ''
-        
+        values = {'action':'query'}
+        if titles != []:
+            values['titles'] = self.listToString(titles)
+        elif pageIds != []:
+            values['pageids'] = self.listToString(pageIds)
+        elif revIds != []:
+            values['revids'] = self.listToString(revIds)
+        elif list != []:
+            values['list'] = self.listToString(list)
+        elif meta != []:
+            values['meta'] = self.listToString(meta)
+
+        for key, value in extraParams.items():
+            values[key] = value
+
+        if generator != '':
+            values['generator'] = generator
+
+        if redirects == True:
+            values['redirects'] = ''
+
         if convertTitles == True:
             values['converttitles'] = ''
-        
+
         if indexPageIds == True:
             values['indexpageids'] = ''
-        
+
         if export == True:
             values['export'] = ''
-        
+
         if exportNoWrap == True:
             values['exportnowrap'] = ''
-        
+
         if iwUrl == True:
             values['iwurl'] = ''
-        
+
         return self.apiRequest(values)
-    
+
     def expandTemplates(self):
         pass
-    
+
     def parse(self):
         pass
-    
+
     def openSearch(self):
         pass
-    
+
     def feedContributions(self):
         pass
-    
+
     def feedWatchlist(self):
         pass
-    
+
     def help(self):
         pass
-    
+
     def paramInfo(self):
         pass
-    
+
     def rsd(self):
         pass
-    
+
     def compare(self):
         pass
-    
+
     def purge(self):
         pass
-    
+
     def rollback(self):
         pass
-    
+
     def delete(self, title = None, pageId = None, reason = None, oldImage = None):
         #get a delete token
         result = self.apiRequest({'action':'query', 'prop':'info', 'intoken':'delete', 'titles':'Main Page'})
@@ -136,67 +151,67 @@ class MediaWikiClient:
             values['pageid'] = pageId
         else:
             raise Error, "You must chose a title or a page ID."
-        
+
         if reason != None:
             values['reason'] = reason
-        
+
         if oldImage == True:
             values['oldimage'] = ''
-        
+
         result = self.apiRequest(values)
-    
+
     def undelete(self):
         pass
-    
+
     def protect(self):
         pass
-    
+
     def block(self, user, reason = None, expiry = "infinite", noCreate = True, noEmail = False, autoBlock = True, anonOnly = False):
         values = {'action':'block', 'user':user, 'expiry':expiry}
         if noCreate == True:
             values['nocreate'] = ''
-        
+
         if noEmail == True:
             values['noemail'] = ''
-        
+
         if autoBlock == True:
             values['autoblock'] = ''
-        
+
         if anonOnly == True:
             values['anononly'] = ''
-        
+
         if reason != None:
             values['reason'] = reason
         result = self.apiRequest(values)
 
     def unblock(self):
         pass
-    
+
     def move(self):
         pass
-    
+
     def edit(self):
         pass
-    
+
     def upload(self):
         pass
-    
+
     def filerevert(self):
         pass
-    
+
     def watch(self):
         pass
-    
+
     def patrol(self):
         pass
-    
+
     def _import(self):
         pass
-    
+
     def userrights(self, user, add = [], remove = [], reason = None):
         values = {'action':'query', 'list':'users', 'ususers':user, 'ustoken':'userrights'}
         token = self.apiRequest(values)['query']['users'][0]['userrightstoken']
-        
+
         headers = {'action':'userrights', 'user':user, 'add':self.listToString(add), 'remove':self.listToString(remove), 'token':token}
         if reason is not None:
             headers['reason'] = reason
